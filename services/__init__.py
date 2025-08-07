c67f1e1209f1929e0fd7e85b34232676494428f2
Hyperiongate <jim@shift-work.com>
2025-08-07 11:22:19 -0700

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,31 +1,5 @@
-"""
-Create this file as services/__init__.py
-This ensures all services can be imported properly
-"""
-
 # This file makes the services directory a Python package
-# It can be empty, but we'll import all services to ensure they're available
+# Keep it EMPTY to avoid circular imports
+# The service_registry will handle all imports dynamically
 
-# Import all service classes to ensure they're accessible
-from .base_analyzer import BaseAnalyzer, AsyncBaseAnalyzer
-from .article_extractor import ArticleExtractor
-from .source_credibility import SourceCredibility
-from .author_analyzer import AuthorAnalyzer
-from .bias_detector import BiasDetector
-from .fact_checker import FactChecker
-from .transparency_analyzer import TransparencyAnalyzer
-from .manipulation_detector import ManipulationDetector
-from .content_analyzer import ContentAnalyzer
-
-__all__ = [
-    'BaseAnalyzer',
-    'AsyncBaseAnalyzer',
-    'ArticleExtractor',
-    'SourceCredibility', 
-    'AuthorAnalyzer',
-    'BiasDetector',
-    'FactChecker',
-    'TransparencyAnalyzer',
-    'ManipulationDetector',
-    'ContentAnalyzer'
-]
+# DO NOT import services here - it causes circular import issues!
