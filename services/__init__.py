fc24355f7c8cf90c75eed27f3606e5c14bf4629f
Hyperiongate <jim@shift-work.com>
2025-08-07 07:48:48 -0700

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,12 +1,31 @@
 """
-FILE: services/__init__.py
-PURPOSE: Makes the services directory a Python package
-DEPENDENCIES: None
+Create this file as services/__init__.py
+This ensures all services can be imported properly
 """
 
-# Version information
-__version__ = "1.0.0"
-__service__ = "news-analyzer"
+# This file makes the services directory a Python package
+# It can be empty, but we'll import all services to ensure they're available
 
-# Note: We don't import anything here to avoid circular imports
-# Each service should be imported directly when needed
+# Import all service classes to ensure they're accessible
+from .base_analyzer import BaseAnalyzer, AsyncBaseAnalyzer
+from .article_extractor import ArticleExtractor
+from .source_credibility import SourceCredibility
+from .author_analyzer import AuthorAnalyzer
+from .bias_detector import BiasDetector
+from .fact_checker import FactChecker
+from .transparency_analyzer import TransparencyAnalyzer
+from .manipulation_detector import ManipulationDetector
+from .content_analyzer import ContentAnalyzer
+
+__all__ = [
+    'BaseAnalyzer',
+    'AsyncBaseAnalyzer',
+    'ArticleExtractor',
+    'SourceCredibility', 
+    'AuthorAnalyzer',
+    'BiasDetector',
+    'FactChecker',
+    'TransparencyAnalyzer',
+    'ManipulationDetector',
+    'ContentAnalyzer'
+]
