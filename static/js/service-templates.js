3a61e01a175be3543dfa985f6569f7fa7ed881e8
Hyperiongate <jim@shift-work.com>
2025-09-04 13:46:40 -0700

    Create service-templates.js

---
+++ static/js/service-templates.js
@@ -0,0 +1,11 @@
+// service-templates.js - Service Template Definitions for TruthLens
+// This file can be empty if templates are handled in app.js
+// Or you can move service-specific templates here
+
+window.ServiceTemplates = {
+    // Templates are now defined directly in app.js
+    // This file exists to prevent 404 errors
+    initialized: true
+};
+
+console.log('Service templates loaded');
